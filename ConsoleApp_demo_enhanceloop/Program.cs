33efa727b25c322311252d508904cd1241c0bd93
niles <niles@LAPTOP-3K24CQ1R>
2021-08-29 23:16:27 +0530

    Add project files.

---
+++ ConsoleApp_demo_enhanceloop/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp_demo_enhanceloop
+{
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] arr;
+
+            arr = new string[10];
+            arr[0] = "aa";
+            arr[1] = "bb";
+            arr[2] = "cc";
+
+            Console.WriteLine("Elements of the array : ");
+
+            foreach (string val in arr)
+            {
+                Console.WriteLine(val);
+            }
+
+            Console.ReadLine();
+
+
+            /*
+            int[] lst = { 23, 34, 45, 56 };
+            foreach(int x in lst)
+            {
+                Console.WriteLine(x);
+            }
+            */
+
+        }
+    }
+}
